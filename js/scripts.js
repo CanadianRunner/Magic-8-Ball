56849ed329c19815b486fe036955721ce799175a
Sean Keane <seankeane93@gmail.com>
2022-04-03 12:07:26 -0700

    add variables and math function

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let userName = 'Sean';
+
+userName ? console.log(`Hello, ${userName}`) : console.log('Hello!');
+
+let userQuestion = 'Will I win the lottery in the next three days?';
+
+console.log(`${userName} has asked - ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+console.log(randomNumber);
