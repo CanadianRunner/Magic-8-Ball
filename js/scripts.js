b9a85baea44d575aa1701f96601bc4ad7e43e7da
Sean Keane <seankeane93@gmail.com>
2022-04-03 15:30:27 -0700

    add string outcomes and log responses

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,31 @@ console.log(`${userName} has asked - ${userQuestion}`);
 let randomNumber = Math.floor(Math.random() * 8);
 
 console.log(randomNumber);
+
+let eightBall = '';
+
+switch (randomNumber) {
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1:
+        eightBall = 'It is decidely so';
+        break;
+    case 3:
+        eightBall = 'Reply hazy try again';
+        break;
+    case 4:
+        eightBall = 'Cannot perdict now';
+        break;
+    case 5:
+        eightBall = 'My sources say no';
+        break;
+    case 6:
+        eightBall = 'Outlook not so good';
+        break;
+    case 7:
+        eightBall = 'Signs point to yes';
+        break;
+}
+
+console.log(`The magic eightball says, ${eightBall}.`)
